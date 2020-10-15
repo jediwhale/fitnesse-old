c8b1e86a2257f5084f82af81caa2f0867f02dba9
Mike Stockdale <mikes@syterra.com>
2020-10-14 22:17:26 -0600

    translate see

--- src/fitnesse/wikitext/parser3/See.java
+++ src/fitnesse/wikitext/parser3/See.java
@@ -1,7 +1,9 @@
 package fitnesse.wikitext.parser3;
 
+import fitnesse.html.HtmlTag;
+
 public class See {
   public static String translate(Symbol symbol, Translator translator) {
-    return "see";
+    return new HtmlTag("b").body("See: " + translator.translate(symbol.getChild(0))).htmlInline();
   }
 }
--- test/fitnesse/wikitext/parser3/KeywordTest.java
+++ test/fitnesse/wikitext/parser3/KeywordTest.java
@@ -2,22 +2,22 @@ package fitnesse.wikitext.parser3;
 
 import org.junit.Test;
 
-import static fitnesse.wikitext.parser3.Helper.assertParses;
-import static fitnesse.wikitext.parser3.Helper.assertScans;
+import static fitnesse.wikitext.parser3.Helper.*;
 
 public class KeywordTest {
-  @Test
-  public void scansNote() {
+  @Test public void scansNote() {
     assertScans("Note=!note ,Text=something", "!note something");
   }
 
-  @Test
-  public void scansSee() {
+  @Test public void scansSee() {
     assertScans("See=!see ,Text=something", "!see something");
   }
 
-  @Test
-  public void parsesSee() {
-    assertParses("SEE(TEXT=stuff)", "!see stuff");
+  @Test public void parsesSee() {
+    assertParses("SEE(WIKI_LINK=MyPage)", "!see MyPage");
+  }
+
+  @Test public void translatesSee() {
+    assertTranslates("<b>See: <a href=\"Fake.MyPage\">MyPage</a></b>", "!see MyPage");
   }
 }
