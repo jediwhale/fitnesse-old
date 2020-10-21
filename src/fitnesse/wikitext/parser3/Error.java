4e2a665a5f3f6dd2af0fdcd18aedb5d3643ca91d
Mike Stockdale <mikes@syterra.com>
2020-10-21 16:23:26 -0600

    change css class for parse errors

--- src/fitnesse/wikitext/parser3/Error.java
+++ src/fitnesse/wikitext/parser3/Error.java
@@ -4,6 +4,6 @@ import fitnesse.html.HtmlTag;
 
 public class Error {
   public static String translate(Symbol symbol, Translator translator) {
-    return " " + HtmlTag.name("span").attribute("class", "error").body(symbol.translateContent(translator)).htmlInline() + " ";
+    return " " + HtmlTag.name("span").attribute("class", "fail").body(symbol.translateContent(translator)).htmlInline() + " ";
   }
 }
